24a3f5f54bb7ae91e612f80ba67f77f5c4d4dbeb
Moises Munoz <moises_a_munoz@yahoo.com>
2018-04-03 13:14:22 -0700

    Added top navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './navbar.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+
+export default class Example extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light expand="md">
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="#">Projects</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Gaming</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          <NavbarBrand href="/">Moisés Muñoz</NavbarBrand>
+        </Navbar>
+      </div>
+    );
+  }
+}
